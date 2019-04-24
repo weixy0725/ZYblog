68081ddda133aeaa64b9724260ee39ff0845be4b
weixy0725 <weixy0725@sina.com>
2019-04-24 09:16:29 +0800

    第一次提交

---
+++ src/main/java/com/zhiyu/blog/ZYblogApplication.java
@@ -0,0 +1,13 @@
+package com.zhiyu.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZYblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZYblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhiyu/blog/ZYblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhiyu.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZYblogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
